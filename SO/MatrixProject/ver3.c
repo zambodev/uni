e22e9e24022f2e4be40b59978b117ed6ea454e03
Davoleo <dav@davoleo.net>
2022-01-19 12:01:20 +0100

    Fix some formatting inconsistencies and variable scopes

--- SO/MatrixProject/ver3.c
+++ SO/MatrixProject/ver3.c
@@ -21,24 +21,19 @@ int matrix_length;
 int cords_arr[MATRIX_SIZE * MATRIX_SIZE][2];
 
 
-void compute_cell(int cords[2])
-{	
+void compute_cell(int cords[2]) {	
 	int x = cords[0];
 	int y = cords[1];
-	long val1, val2, result;
-	int idx;
 
 	int thread_join_idx = 1;
 
-	if(y == 0)
-	{
-		for(int i=1; i < matrix_length; i++)
-		{
-                        int shift_idx = x * matrix_length;
-                        cords_arr[shift_idx+i][0] = x;
-                        cords_arr[shift_idx+i][1] = i;
-                        
-                        if(pthread_create(&thread_arr[x][i], NULL, (void *)compute_cell, (void *)cords_arr[shift_idx+i]) != 0)
+	if(y == 0) {
+		for(int i=1; i < matrix_length; i++) {
+			int shift_idx = x * matrix_length;
+			cords_arr[shift_idx+i][0] = x;
+			cords_arr[shift_idx+i][1] = i;
+						
+			if(pthread_create(&thread_arr[x][i], NULL, (void*) compute_cell, (void*) cords_arr[shift_idx+i]) != 0)
 				i--;
 
 			if(pthread_join(thread_arr[x][thread_join_idx], NULL) == 0)
@@ -47,68 +42,67 @@ void compute_cell(int cords[2])
 	}
 
 
+	long val1, val2;
 	val1 = matrix1[x][y];
 
-	for(int i=0; i < matrix_length; i++)
-	{
-		idx = (i+y) % matrix_length;
+	for(int i=0; i < matrix_length; i++) {
+		int hyb = (i+y) % matrix_length;
 
-		val2 = matrix2[y][idx];
-		result = val1 * val2;
+		val2 = matrix2[y][hyb];
 
-		pthread_mutex_lock(&matrix_res_lock[x][idx]);
-		matrix_res[x][idx] += result;
-		pthread_mutex_unlock(&matrix_res_lock[x][idx]);
+		pthread_mutex_lock(&matrix_res_lock[x][hyb]);
+		matrix_res[x][hyb] += val1 * val2;
+		pthread_mutex_unlock(&matrix_res_lock[x][hyb]);
 	}
-        
-        if(y == 0)
-        {
-                for(int i=thread_join_idx; i < matrix_length; i++)
-                        while(pthread_join(thread_arr[x][i], NULL) != 0) {}
-        }
-        
-        pthread_exit(NULL);
+		
+	if(y == 0) {
+		for(int i=thread_join_idx; i < matrix_length; i++)
+			while(pthread_join(thread_arr[x][i], NULL) != 0) { 
+				/* wait until all threads are joined*/ 
+			}
+	}
+
+	pthread_exit(NULL);
 }
 
 
 
 int main()
 {	
-	/* Load matrices */
+	/* Load Matrices */
 	load_matrix(matrix1, &matrix_length, "matrice100-1.txt");
 	load_matrix(matrix2, &matrix_length, "matrice100-2.txt");
 
-	/* Setup mutex */
+	/* Setup Heap Allocated array of mutexes */
 	matrix_res_lock = malloc(sizeof(pthread_mutex_t) * matrix_length);
-	for(int x=0; x < matrix_length; x++)
-	{
+	for(int x=0; x < matrix_length; x++) {
 		matrix_res_lock[x] = malloc(sizeof(pthread_mutex_t) * matrix_length);
 
 		for(int y=0; y < matrix_length; y++)
 			pthread_mutex_init(&matrix_res_lock[x][y], NULL);
 	}
 
+	/* --- Initial Benchmarking checkpoint (COMPUTATION STARTS HERE) --- */
 	struct timespec start, end;
 	clock_gettime(CLOCK_REALTIME, &start);
 	
 	/* Start threads */
-	for(int x=0; x < matrix_length; x++)
-	{
+	for(int x=0; x < matrix_length; x++) {
 		cords_arr[x*matrix_length][0] = x;
 
-		if(pthread_create(&thread_arr[x][0], NULL, (void *)compute_cell, (void *)cords_arr[x*matrix_length]) != 0)
-		{	
+		if(pthread_create(&thread_arr[x][0], NULL, (void*) compute_cell, (void*) cords_arr[x*matrix_length]) != 0) {	
 			puts("Thread creation failed!");
 			return EXIT_FAILURE;
 		}
 	}
 
-	/* Wait for evey thread to join */
-	for(int x=0; x < matrix_length; x++)
-	{
-		while(pthread_join(thread_arr[x][0], NULL) != 0) {}
+	for(int x=0; x < matrix_length; x++) {
+		while(pthread_join(thread_arr[x][0], NULL) != 0) {
+			/* Wait for evey thread to join */
+		}
 	}
 
+	/* --- Final Benchmarking checkpoint (COMPUTATION ENDS HERE) --- */
 	clock_gettime(CLOCK_REALTIME, &end);
 
 	/* Print result matrix */
@@ -117,9 +111,8 @@ int main()
 	/* Print execution time */
 	printf("Execution time: %0.3f sec\n", (end.tv_sec - start.tv_sec) + 1e-9*(end.tv_nsec - start.tv_nsec));
 
-	/* Destroy mutex */
-	for(int x=0; x < matrix_length; x++)
-	{
+	/* Destroy mutexes */
+	for(int x=0; x < matrix_length; x++) {
 		for(int y=0; y < matrix_length; y++)
 			pthread_mutex_destroy(&matrix_res_lock[x][y]);
 
